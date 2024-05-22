dffc35409abb7f3ec6fb5df36a23319888292cef
KatkarAnkitha <102357083+KatkarAnkitha@users.noreply.github.com>
2024-05-22 17:32:18 +0530

    Add files via upload

---
+++ numberpyramid.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main
+{
+public static void main(String[] args)
+{
+ int i,j,k,m=1,num;
+Scanner sc=new Scanner(System.in);
+System.out.print("enter number of rows:");
+num=sc.nextInt();
+for(i=1;i<=num;i++)
+{
+for(j=1;j<=i;j++)
+{System.out.print(i+" ");
+
+}
+System.out.println();
+}
+}
+}
